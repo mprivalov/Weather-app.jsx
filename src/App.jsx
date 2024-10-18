faa972c62eeace6c04d0dd9d9338ccb54bbb9d02
mprivalov <mar4eloprivalov@gmail.com>
2024-10-18 09:53:50 +0300

    React Weather App

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TempAndDetails from "./components/TempAndDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  const [query, setQuery] = useState({ q: "london" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  const getWeather = async () => {
+    const message = query.q ? query.q : 'qurrent location';
+    toast.info (`Fetching weather data for ${capitalizeFirstLetter(message)}`);
+
+    await getFormattedWeatherData({ ...query, units }).then((data) => {
+      toast.success(`Fetched weather data for ${data.name}, ${data.country}`)
+      setWeather(data);
+    });
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-600 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-600 to-blue-700";
+    return "from-yellow-600 to-orange-700";
+  };
+
+  return (
+    <div
+      className={`mx-auto max-w-screen-lg my-4 py-5 px-32 bg-gradient-to-br shadow-sm shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} setUnits={setUnits} />
+
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TempAndDetails weather={weather} units={units} />
+          <Forecast title="3 hour step forecast" data={weather.hourly} />
+          <Forecast title="daily forecast" data={weather.daily} />
+        </>
+      )}
+
+      <ToastContainer autoClose={1000} hideProgressBar={true} theme="colored" />
+    </div>
+  );
+};
+
+export default App;
